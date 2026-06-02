fb4f6b26997302dea224d1b006a6befae559f78e
Shruti Nair <shruti.pathmini@gmail.com>
2026-06-02 22:31:38 +0100

    Added new files to day6

---
+++ day6/ReverseArray.java
@@ -0,0 +1,10 @@
+package day6;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int a[] = {10, 20, 30, 40, 50};
+        for(int i = 4; i>=0; i--) {
+            System.out.println(a[i]);
+        }
+    }
+}
